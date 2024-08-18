d5d4a19e2ac30ee2cf367642659abe2cc8fc1076
Mobeen Qasim <mobeenqq@gmail.com>
2024-08-18 18:32:15 -0400

    apple website

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-sg",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
+})
